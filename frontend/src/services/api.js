51585d2d90dee8178e5b1f7d3073166c6c16f25a
Burhan5153 <burhanmkanch@gmail.com>
2026-04-04 00:15:54 +0500

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://your-url.up.railway.app/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://backend-production-2240.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json'
   }
